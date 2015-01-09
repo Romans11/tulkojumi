073bad214e34a2f70344f647a5500b665cf40914
Romans11 <romanskolduns@inbox.lv>
2015-01-09 21:34:23 +0200

    Kategoriju uzlabojumi

--- info.php
+++ info.php
@@ -38,7 +38,7 @@ while($row = mysql_fetch_array($result))
 	echo("<big><b><span title=Latviešu>Latviešu</span></b>: ".$row['lv']."<br/>");
 	echo("<b><span title=English>Angļu</span></b>: ".$row['en']."<br/>");
 	echo("<b><span title=Русский>Krievu</span></b>: ".$row['ru']."<br/>");
-	$c=$row['i'];
+	$c=$row['kat'];
 }
 echo "</table><br/>";
 mysql_close($db_con);
@@ -51,57 +51,57 @@ if (strlen($c)==0)
 echo("<br/><h2>Kategorijas</h2><big>");
 function nosen($k)
 {
-if ($k=="-l") $k="Noun";
-if ($k=="-d") $k="Verb";
-if ($k=="-i") $k="Adjective";
-if ($k=="-v") $k="Pronoun";
-if ($k=="-a") $k="Adverb";
-if ($k=="-s") $k="Numeral";
-if ($k=="-n") $k="Proper name";
-if ($k=="-c") $k="Other";
-if ($k=="?f") $k="Phrase";
-if ($k=="?j") $k="Question";
-if ($k=="?t") $k="Sentence";
-if ($k==".i") $k="Everyday situation";
-if ($k==".o") $k="Formal conversation";
-if ($k==".c") $k="Casual conversation";
-return $k;
+	if ($k=="-l") $k="Noun";
+	if ($k=="-d") $k="Verb";
+	if ($k=="-i") $k="Adjective";
+	if ($k=="-v") $k="Pronoun";
+	if ($k=="-a") $k="Adverb";
+	if ($k=="-s") $k="Numeral";
+	if ($k=="-n") $k="Proper name";
+	if ($k=="-c") $k="Other";
+	if ($k=="?f") $k="Phrase";
+	if ($k=="?j") $k="Question";
+	if ($k=="?t") $k="Sentence";
+	if ($k==".i") $k="Everyday situation";
+	if ($k==".o") $k="Formal conversation";
+	if ($k==".c") $k="Casual conversation";
+	return $k;
 }
 function noslv($k)
 {
-if ($k=="-l") $k="Lietvārds";
-if ($k=="-d") $k="Darbības vārds";
-if ($k=="-i") $k="Īpašības vārds";
-if ($k=="-v") $k="Vietniekvārds";
-if ($k=="-a") $k="Apstākļa vārds";
-if ($k=="-s") $k="Skaitļa vārds";
-if ($k=="-n") $k="Īpašvārds";
-if ($k=="-c") $k="Cits";
-if ($k=="?f") $k="Frāze";
-if ($k=="?j") $k="Jautājums";
-if ($k=="?t") $k="Teikums";
-if ($k==".i") $k="Ikdienas situācija";
-if ($k==".o") $k="Oficiāla saruna";
-if ($k==".c") $k="Neoficiāla saruna";
-return $k;
+	if ($k=="-l") $k="Lietvārds";
+	if ($k=="-d") $k="Darbības vārds";
+	if ($k=="-i") $k="Īpašības vārds";
+	if ($k=="-v") $k="Vietniekvārds";
+	if ($k=="-a") $k="Apstākļa vārds";
+	if ($k=="-s") $k="Skaitļa vārds";
+	if ($k=="-n") $k="Īpašvārds";
+	if ($k=="-c") $k="Cits";
+	if ($k=="?f") $k="Frāze";
+	if ($k=="?j") $k="Jautājums";
+	if ($k=="?t") $k="Teikums";
+	if ($k==".i") $k="Ikdienas situācija";
+	if ($k==".o") $k="Oficiāla saruna";
+	if ($k==".c") $k="Neoficiāla saruna";
+	return $k;
 }
 function nosru($k)
 {
-if ($k=="-l") $k="Существительное";
-if ($k=="-d") $k="Глагол";
-if ($k=="-i") $k="Прилагательное";
-if ($k=="-v") $k="Местоимение";
-if ($k=="-a") $k="Наречие";
-if ($k=="-s") $k="Число";
-if ($k=="-n") $k="Имя собственное";
-if ($k=="-c") $k="Другoe";
-if ($k=="?f") $k="Фраза";
-if ($k=="?j") $k="Вопрос";
-if ($k=="?t") $k="Предложение";
-if ($k==".i") $k="Повседневная ситуация";
-if ($k==".o") $k="Формальный разговор";
-if ($k==".c") $k="Неформальный разговор";
-return $k;
+	if ($k=="-l") $k="Существительное";
+	if ($k=="-d") $k="Глагол";
+	if ($k=="-i") $k="Прилагательное";
+	if ($k=="-v") $k="Местоимение";
+	if ($k=="-a") $k="Наречие";
+	if ($k=="-s") $k="Число";
+	if ($k=="-n") $k="Имя собственное";
+	if ($k=="-c") $k="Другoe";
+	if ($k=="?f") $k="Фраза";
+	if ($k=="?j") $k="Вопрос";
+	if ($k=="?t") $k="Предложение";
+	if ($k==".i") $k="Повседневная ситуация";
+	if ($k==".o") $k="Формальный разговор";
+	if ($k==".c") $k="Неформальный разговор";
+	return $k;
 }
 if (strlen($c)==2)
 {
@@ -110,19 +110,17 @@ if (strlen($c)==2)
 	echo ("<tr><td><b>Русский</b></td><td>" . nosru($c) . "</td></tr></table></big>");
 	Exit();
 }
-if (strlen($c)==4){
-	Exit();
-}
-if (strlen($c)==6){
-	Exit();
-}
-if (strlen($c)==8){
-	Exit();
-}
-if (strlen($c)==10){
-	Exit();
-}
-if (strlen($c)==12){
+if (strlen($c)>2)
+{
+	$tmpkat="";
+	for($i=0 ; $i<strlen($c)/2 ; $i++)
+	{
+		$tmpkat=substr($c,$i*2,2);
+		echo ("<table border=0><tr><td><b>Latviešu</b></td><td>" . noslv($tmpkat) . "</td></tr>");
+		echo ("<tr><td><b>English</b></td><td>" . nosen($tmpkat) . "</td></tr>");
+		echo ("<tr><td><b>Русский</b></td><td>" . nosru($tmpkat) . "</td></tr></table><hr/>");
+	}
+	echo ("</big>");
 	Exit();
 }
 ?>
--- tulkojumi.sql
+++ tulkojumi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 -- 
 -- Hosts: localhost
--- Izveidošanas laiks: 09.01.2015 20:54
+-- Izveidošanas laiks: 09.01.2015 21:33
 -- Servera versija: 5.0.51
 -- PHP Versija: 5.2.6
 
@@ -58,7 +58,7 @@ INSERT INTO `vardi` VALUES (21, 'vienpadsmit', 'eleven', 'одиннадцать
 INSERT INTO `vardi` VALUES (22, 'divpadsmit', 'twelve', 'двенадцать', '-s');
 INSERT INTO `vardi` VALUES (23, 'Uz veselību!', 'Bless you (when szeezing)', 'Будьте здоровы!', '.i');
 INSERT INTO `vardi` VALUES (24, 'atvainojiet!', 'excuse me!', 'извините!', '.o');
-INSERT INTO `vardi` VALUES (25, 'Ko tas nozīmē?', 'What does it mean?', 'Что это значит?', '.o');
+INSERT INTO `vardi` VALUES (25, 'Ko tas nozīmē?', 'What does it mean?', 'Что это значит?', '.o?j');
 INSERT INTO `vardi` VALUES (26, 'piektdien', 'on Friday', 'в пятницу', '-a');
 INSERT INTO `vardi` VALUES (27, 'pirmdien', 'on Monday', 'в понедельник', '-a');
 INSERT INTO `vardi` VALUES (28, 'sestdien', 'on Saturday', 'в субботу', '-a');
@@ -67,7 +67,7 @@ INSERT INTO `vardi` VALUES (30, 'ceturtdien', 'on Thursday', 'в четверг'
 INSERT INTO `vardi` VALUES (31, 'otrdien', 'on Tuesday', 'во вторник', '-a');
 INSERT INTO `vardi` VALUES (32, 'trešdien', 'on Wednesday', 'в среду', '-a');
 INSERT INTO `vardi` VALUES (33, 'Kāda ir jūsu dzimtā valoda?', 'What is your native language?', 'Каков ваш родной язык?', '?j');
-INSERT INTO `vardi` VALUES (34, 'Cik pulkstens?', 'What time is it?', 'Сколько сейчас времени?', '.a?j');
+INSERT INTO `vardi` VALUES (34, 'Cik pulkstens?', 'What time is it?', 'Сколько сейчас времени?', '?j.i');
 INSERT INTO `vardi` VALUES (35, 'jā', 'yes', 'да', '-a');
 INSERT INTO `vardi` VALUES (36, 'nē', 'no', 'нет', '-a');
 INSERT INTO `vardi` VALUES (37, 'labi', 'good ; ok', 'хорошо', '-a');
